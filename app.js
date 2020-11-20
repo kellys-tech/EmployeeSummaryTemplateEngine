caa600ac48d79de955b04df7b96616ac7c336ade
Kelly Smith <kdalton77@hotmail.com>
2020-11-20 11:06:37 -0500

    call employee role (empRole) function

--- app.js
+++ app.js
@@ -192,6 +192,8 @@ function renderSite() {
     }
 }
 
+switchStatement();
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
